6f124c8c6118b114073551bd77084292b418c6bf
Juma <gerald.juma@student.moringaschool.com>
2025-03-11 14:26:53 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+
+
+const divide = function(dividend, divisor) {
+    return dividend / divisor;
+  };
+  
+
+  console.log(divide(2000, 100));
+  
+
+  const square = (number) => {
+    return number * number;
+  };
+  
+  console.log(square(5)); 
+  
+  
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+
+  console.log(add(10, 20)); 
